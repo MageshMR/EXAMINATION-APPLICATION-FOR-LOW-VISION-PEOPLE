093f4bd67000d7dbec7667e50d44ed83fa9eae56
MageshMR <119123598+MageshMR@users.noreply.github.com>
2025-05-06 12:14:11 +0530

    Update wsgi.py

--- visionfree_exam/visionfree_exam/wsgi.py
+++ visionfree_exam/visionfree_exam/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visionfree_exam.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visionfree_exam.visionfree_exam.settings')
 
 application = get_wsgi_application()
